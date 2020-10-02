4afabf985e9cc0ce3b89c8a0ed5b747f77deeeb2
Danil Larin <PickleDan@yandex.ru>
2020-10-02 21:31:06 +0300

    add bootstrap, header layout

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
+type FooterProps = {};
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className="text-center py-3">Copyright &copy; DShop</Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
